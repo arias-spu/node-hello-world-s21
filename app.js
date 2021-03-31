e9f1e9218001f7192097948e3e92523d5c705c63
Carlos R. Arias <arias@spu.edu>
2021-03-31 15:25:24 -0700

    finished app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer((request, response) => {
+	response.statusCode = 200;
+	response.setHeader('Content-Type', 'text/plain');
+	response.end('Hello World\n\n\nYou Guys Rock!');
+});
+
+server.listen(port, (err) => {
+	if (err) {
+		return console.log('Something is wrong shut the lights');
+	}
+	console.log(`Server Listening on : ${port}`);
+}
+
